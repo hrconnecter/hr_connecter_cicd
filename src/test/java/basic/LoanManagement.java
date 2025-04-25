1ce6a900f4c82058cfbaec63987b94245e7f3e4f
Rahul Varne <Rahulva@ats.com>
2025-04-25 14:56:24 +0530

    New Changes

--- src/test/java/basic/LoanManagement.java
+++ src/test/java/basic/LoanManagement.java
@@ -10,101 +10,71 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 public class LoanManagement                                                                            
 {    
-	public static WebDriver driver;
-	
+	public static WebDriver driver;	
 	@BeforeClass
-	public void preCondition1() throws IOException, InterruptedException
+	public void preCondition() throws IOException
 	{   
-
-	String url = "https://app.aegishrms.com/sign-in";
 		System.out.println("<---Automation Script For Loan Management Start--->");
-		Thread.sleep(2000);
-		driver = new ChromeDriver();
+		String browser = "edge";
+		String url = "https://app.aegishrms.com/sign-in";
+		if(browser.equalsIgnoreCase("chrome"))
+		{
+			driver = new ChromeDriver();
+		}
+		else if(browser.equalsIgnoreCase("edge"))   
+		{
+			driver = new EdgeDriver();
+		}  
+		else   
+		{
+			driver = new ChromeDriver();
+		} 	
+		System.out.println("<-----Chrome Browser Is Launched------>");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
 		driver.get(url);
-		System.out.println("<--- Web Application URL Is Entered--->");
+		System.out.println("<--- Web Application URL Is Entered--->");	
 	}
-//	public void preCondition() throws IOException ,InterruptedException
-//	{   
-//		System.out.println("<---Automation Script For Loan Management Start--->");
-//		Thread.sleep(2000);
-//		String browser = "chrome";
-//		String url = "https://app.aegishrms.com/sign-in";
-//				
-//		if(browser.equalsIgnoreCase("chrome"))
-//		{
-//			driver = new ChromeDriver();
-//		}
-//		else if(browser.equalsIgnoreCase("edge"))   
-//		{
-//			driver = new EdgeDriver();
-//		}  
-//		else   
-//		{
-//			driver = new ChromeDriver();
-//		} 	
-//		System.out.println("<-----Chrome Browser Is Launched------>");
-//		driver.manage().window().maximize();
-//		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
-//		driver.get(url);
-//		System.out.println("<--- Web Application URL Is Entered--->");	
-//	}
-	
 	@Test
 	public void basicOrganisationLoanManagement() throws InterruptedException
 	{
 		Login("noble@gmail.com", "Pass@123");   
 		System.out.println("<---Super Admin Login--->");
-		
 		loanManagementSetup("Bike Loan","0","100000","1");
 		System.out.println("<---Loan Management Setup--->");	
-		
 		Logout(); 
 		System.out.print("<---Super Admin Logout--->");
-		
 		Login("sahil@basic.com","Pass@123"); 
 		System.out.print("<---Employee Login--->");
-		
 		employeeLoanApply();
 		System.out.println("<---Employee Apply For Loan--->");
-		
 		loanApplicationForm("2000", "2");
 		System.out.println("<---Loan Application Form--->");
-		
 		Logout();
 		System.out.println("<---Employee Logout--->");
-		
 		Login("hr@basic.com","Pass@123");
 		System.out.println("<---Hr User Login--->"); 	
-		
 		hr_Loan_Approvel();
 		System.out.println("<---Hr Loan Approvel--->");
-		
 		Logout();
 		System.out.println("<---Hr User Logout--->");
-		
 		Login("noble@gmail.com", "Pass@123");   
 		System.out.println("<---Super Admin Login--->"); 
-		
 		superAdminRemoveLoanType();
 		System.out.println("<---Super Admin Remove Loan Type-->");
-		
 		Logout(); 
 		System.out.print("<---Super Admin Logout--->");	
 	}  
-	
 	@AfterClass
 	public void postConditions() throws InterruptedException
 	{
 		closeServer();
 	}
-	
 	 //<-------------------------User defined Methods------------------------>\\
-
+ 
 	public void loanManagementSetup(String loanName,String minLoanAmount,String maxLoanAmount,String intrest) throws InterruptedException
 	{
-		Thread.sleep(1000);
+		Thread.sleep(2000);
 		driver.findElement(By.xpath("//button[text()='Go To Organisation']")).click();
 		driver.findElement(By.xpath("(//button[text()='Setup'])[2]")).click();
 		driver.findElement(By.xpath("//h1[text()='Loan Management']")).click();
@@ -116,20 +86,19 @@ public class LoanManagement
 		Thread.sleep(1000); 
 		driver.findElement(By.xpath("//button[text()='Submit']")).click(); 
 	}
-	 	
 	public void employeeLoanApply() throws InterruptedException
 	{
+		Thread.sleep(2000);
 	    driver.findElement(By.xpath("(//*[local-name()='svg' and @data-testid='MenuIcon'])")).click();
-	    Thread.sleep(500);
+	    Thread.sleep(2000);
 	    driver.findElement(By.xpath("//h1[text()='Payroll']")).click();
-	    Thread.sleep(500);
+	    Thread.sleep(1000);
 	    driver.findElement(By.xpath("(//h1[text()='Loan Management'])[1]")).click();
 	    driver.findElement(By.xpath("//button[text()='Apply For Loan.']")).click();
 	}
-	
 	public void loanApplicationForm(String LoanAmount,String EMI) throws InterruptedException
 	{
-		Thread.sleep(1000);
+		Thread.sleep(2000);
 		driver.findElement(By.xpath("//div[@role='combobox']")).click();
 		driver.findElement(By.xpath("//li[text()='Bike Loan']")).click();
 		Thread.sleep(1000);
@@ -140,9 +109,9 @@ public class LoanManagement
 		driver.findElement(By.xpath("//button[text()='Submit']")).click();
 		Thread.sleep(1000);
 	} 
-	
-	public void hr_Loan_Approvel() throws InterruptedException
+	public void hr_Loan_Approvel() throws InterruptedException   
 	{
+		Thread.sleep(2000);
 		driver.findElement(By.xpath("(//*[local-name()='svg' and @data-testid='NotificationsIcon'])")).click();
 		Thread.sleep(500);
 		driver.findElement(By.xpath("//span[text()='Loan']")).click();
@@ -152,7 +121,6 @@ public class LoanManagement
 		driver.findElement(By.xpath("//button[text()='Accept']")).click();
 		Thread.sleep(500);
 	} 
-	
 	public void superAdminRemoveLoanType() throws InterruptedException
 	{
 		Thread.sleep(1000);
@@ -165,29 +133,23 @@ public class LoanManagement
 		Thread.sleep(500);
 		driver.findElement(By.xpath("//button[text()='Delete']")).click();
 	}
-	
 	public void closeServer() throws InterruptedException
 	{
 		Thread.sleep(2000);
-		driver.quit(); 
+		//driver.quit(); 
 	}
-	
 	public void Login(String Email, String Password) throws InterruptedException
 	{
 		driver.findElement(By.id("email")).sendKeys(Email);
 		driver.findElement(By.id("password")).sendKeys(Password);
-	    driver.findElement(By.xpath("//button[text()='Login']")).click();
-	    Thread.sleep(500);
+		driver.findElement(By.xpath("//button[text()='Login']")).click();
+	    Thread.sleep(1000);
 	}
-	 
 	public void Logout() throws InterruptedException
 	{
+		Thread.sleep(1000);
 		driver.findElement(By.xpath("(//*[local-name()='svg' and @data-testid='PersonIcon'])")).click();
 		Thread.sleep(500);  
 		driver.findElement(By.xpath("//div[contains(text(),' Log out')]")).click();
 	}
 }
-// Manager = sumit@basic.com  
-// Employee = sahil@basic.com  
-// super admin = noble@gmail.com 
-// Hr hr@basic.com
